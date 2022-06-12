3cfd50b2b84a1045510d4af1978d567c2b246c0b
Taegwan Youn <tkyoun0421@naver.com>
2022-06-12 22:05:11 +0900

    Events Part Two

---
+++ js/eventsPartTwo.js
@@ -0,0 +1,19 @@
+// h1 html element MDN
+
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
