139e2ab3535f0c51642761a8f780ea2d6cce49ff
Taegwan Youn <tkyoun0421@naver.com>
2022-06-12 15:00:18 +0900

    Recap

---
+++ js/recap.js
@@ -0,0 +1,17 @@
+const a = null;
+
+// null = 비어있음을 뜻함
+// undefined = 값이 없다는 것을 뜻함
+
+const days = ["Mon" , "Tue" , "Fri" , "Sund"];
+console.log(days);
+// allay = 모든 데이터를 저장한다.
+
+const toBuy = ["potato" , "tomato" , "pizza"];
+console.log(toBuy);
+toBuy[2] = "water";
+console.log(toBuy);
+toBuy.push("one");
+console.log(toBuy);
+
+// .push 배열 안에 내용을 추가시킨다.
