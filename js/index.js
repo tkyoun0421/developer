a72e298eb038402b51c0ada8710bfffb476df59a
Taegwan Youn <tkyoun0421@naver.com>
2022-06-12 14:51:43 +0900

    Functions part Two

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name Is " + nameOfPerson + "  and I'm " + age + " years old")
+}
+
+sayHello("Nico" , 10);
+sayHello("Taegwan" , 30);
+sayHello("HoGeon" , 31);
