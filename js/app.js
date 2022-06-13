146cc9985d14c8f9432012ac1e97d9ead1ec6034
Taegwan Youn <tkyoun0421@naver.com>
2022-06-13 16:03:15 +0900

    Saving Username

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem("username",username);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
