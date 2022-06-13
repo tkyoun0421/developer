ff73c03b01d8690535bde06ba252c41a107ab95d
Taegwan Youn <tkyoun0421@naver.com>
2022-06-13 16:13:54 +0900

    Loading Username

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
 const loginForm = document.querySelector(".login-form")
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector(".greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 // string 이 포함된 변수를 선언할땐 대문자로 선언하는 관습이 있다.
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("username",username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreeting(username);
 }
 
 function handleLinkClick(event) {
     event.preventDefault();
 }
 
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
+}
