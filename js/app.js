d41373ce7c951998feea283519c4e0d916d0bea6
Taegwan Youn <tkyoun0421@naver.com>
2022-06-13 15:55:29 +0900

    Getting Username

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector(".login-form")
 const loginInput = document.querySelector(".login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector(".greeting");
+const HIDDEN_CLASSNAME = "hidden";
+// string 이 포함된 변수를 선언할땐 대문자로 선언하는 관습이 있다.
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
@@ -11,4 +17,3 @@ function handleLinkClick(event) {
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
