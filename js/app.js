d29606931e3a425ea956959d2f786d3c94f6616e
Taegwan Youn <tkyoun0421@naver.com>
2022-06-13 14:58:30 +0900

    Form Submission

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector('.login-form input');
 const loginButton = document.querySelector('.login-form button');
 
 function onLoginBtnClick() {
-    console.log(loginInput.value);
+    const userName = loginInput.value;
+    console.log(userName);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
