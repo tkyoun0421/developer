a6707a6b6f95f4bcacba5e1ae14b5f2e13446a97
Taegwan Youn <tkyoun0421@naver.com>
2022-06-15 19:43:53 +0900

    PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,11 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours());
+    const minutes = String(date.getMinutes());
+    const seconds = String(date.getSeconds());
+    clock.innerText = `${hours.padStart(2,"0")}:${minutes.padStart(2,"0")}:${seconds.padStart(2,"0")}`;
 }
 
 getClock();
-setInterval(getDate,1000);
+setInterval(getClock,1000);
