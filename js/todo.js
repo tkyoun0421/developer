5a6c6c9073967b0c8e1b5d47f2968a4f5748d65d
Taegwan Youn <tkyoun0421@naver.com>
2022-06-16 17:51:59 +0900

    Deleting To Dos part Two

--- js/todo.js
+++ js/todo.js
@@ -16,8 +16,9 @@ function deleteToDo(event) {
 
 function paintToDo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;
+    span.innerText = newToDo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteToDo);
@@ -30,8 +31,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    const newToDoObj = {
+        text: newToDo,
+        id: Date.now(),
+    };
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
