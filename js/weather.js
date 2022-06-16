0b6a528f55f42228210651f7eb9549980f5309a0
Taegwan Youn <tkyoun0421@naver.com>
2022-06-16 18:52:10 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+
+const API_KEY = "6364559631ed8d891e7b1c76ecc05300";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        const name = data.name;
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
